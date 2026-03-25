db6f5f7abf2a14a40d059354fec6e8bb21a0fdfb
Joyce Mo <joycemo413@gmail.com>
2026-03-25 12:36:39 -0700

    Update run_boltz2_guidance.sh
    debugging job script

--- scripts/run_boltz2_guidance.sh
+++ scripts/run_boltz2_guidance.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -cwd
-#$ -j y ## merges stdout and stderr 
-#$ -l mem_free=16G 
+#$ -o boltz_guidance_test_$JOB_ID.o
+#$ -e boltz_guidance_test_$JOB_ID.e
+#$ -o /wynton/home/rotation/jqmo/rotation3/sampleworks/logs
+#$ -l mem_free=16G
 #$ -l scratch=10G
-#$ -l h_rt=48:00:00
+#$ -l h_rt=10:00:00
 #$ -q gpu.q
 #$ -l gpu_mem=16G
 #$ -r y
@@ -39,5 +41,5 @@ pixi run -e boltz python scripts/boltz2_pure_guidance.py \
     --augmentation \
     --align-to-input
 
-## ── End-of-job summary ────────────────────────────────────────────────────────
+## End-of-job summary 
 [[ -n "${JOB_ID:-}" ]] && qstat -j "$JOB_ID"
